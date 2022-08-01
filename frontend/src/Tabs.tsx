5ce83d7e4a448f8c07ac139a6df2dcd790f02b86
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-07-31 22:54:19 -0300

    Frontend: add Chart.js and some layout work

---
+++ frontend/src/Tabs.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Tabs() {
+  const [categories] = useState([
+    "Charts",
+    "Tables",
+    "Geo",
+  ]);
+
+  return (
+    <div className="w-full max-w-sm px-2 sm:px-0">
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-zinc-900 p-1">
+          {categories.map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  "w-full rounded-lg py-2 text-sm font-bold leading-5 focus:outline-none",
+                  selected
+                    ? "bg-zinc-800 shadow font-extrabold text-white/[0.95]"
+                    : "text-zinc-500 hover:bg-white/[0.04] hover:text-white/[0.90]"
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
+    </div>
+  );
+}
