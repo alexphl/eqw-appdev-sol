8bde3c208d927620fdd52cc7508e310c03d9b54f
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-08-20 18:19:01 -0300

    Support rendering multiple charts

--- frontend/pages/Charts.tsx
+++ frontend/pages/Charts.tsx
@@ -1,66 +1,20 @@
 import useSWR from "swr";
 import fetcher from "./fetcher";
-import { Bar } from "react-chartjs-2";
-import { Chart, registerables } from "chart.js";
+import LineChart from "./LineChart";
+import { useState } from "react";
 
 /**
  * Chart visualizations panel
  **/
+
 const Charts = () => {
-  const { data, error } = useSWR("http://localhost:5555/events/daily", fetcher);
-
-  // Shows old data if refresh fails
-  if (error && !data)
-    return (
-      <div>
-        Contents failed to load :( <p> Retrying... </p>
-      </div>
-    );
-
-  Chart.register(...registerables);
+  const [charts, setCharts] = useState([LineChart]);
 
   return (
     <div className="max-w-4xl m-auto">
-      <Bar
-        data={{
-          labels: [],
-          datasets: [
-            {
-              label: "TESTING",
-              data: data,
-              backgroundColor: ["rgba(220, 255, 255, 0.2)"],
-              borderColor: ["rgba(220, 255, 255, 0.6)"],
-              borderWidth: 1,
-              barThickness: 60,
-              borderRadius: 5,
-            },
-          ],
-        }}
-        options={{
-          parsing: {
-            xAxisKey: "date",
-            yAxisKey: "events",
-          },
-          plugins: {
-            legend: {
-              position: "top",
-              align: "center",
-              labels: {
-                boxWidth: 20,
-                usePointStyle: false,
-              },
-              title: {
-                text: "Daily events",
-                display: false,
-                color: "white",
-                font: {
-                  size: 10,
-                },
-              },
-            },
-          },
-        }}
-      />
+      {charts.map((Chart, index) => {
+        return <Chart key={index} />;
+      })}
     </div>
   );
 };
---
+++ frontend/pages/LineChart.tsx
@@ -0,0 +1,67 @@
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+import useSWR from "swr";
+import fetcher from "./fetcher";
+import { useState } from "react";
+
+const LineChart = (props: any) => {
+    const [url, setUrl] = useState("http://localhost:5555/events/daily");
+    const { data, error } = useSWR(url, fetcher);
+
+    // Shows old data if refresh fails
+    if (error && !data)
+        return (
+            <div>
+                Contents failed to load :( <p> Retrying... </p>
+            </div>
+        );
+
+    Chart.register(...registerables);
+
+    return (
+        <Line
+            data={{
+                labels: [],
+                datasets: [
+                    {
+                        label: "Events",
+                        data: data,
+                        backgroundColor: ["rgba(220, 255, 255, 0.2)"],
+                        borderColor: ["rgba(220, 255, 255, 0.6)"],
+                        borderWidth: 1,
+                    },
+                ],
+            }}
+            options={{
+                onClick: function (evt, element) {
+                    if (element.length > 0) {
+                        console.log(element[0].index);
+                        setUrl("http://localhost:5555/events/hourly");
+                    }
+                },
+                animations: {},
+                parsing: {
+                    xAxisKey: "date",
+                    yAxisKey: "events",
+                },
+                elements: {
+                    point: {
+                        radius: 8,
+                        hitRadius: 50,
+                        hoverRadius: 10,
+                    },
+                    line: {
+                        tension: 0.3,
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                },
+            }}
+        />
+    );
+};
+
+export default LineChart;
