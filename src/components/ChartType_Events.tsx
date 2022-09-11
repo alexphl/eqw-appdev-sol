fe573568f64fd9c4a90d1c972c620384c2eae51f
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-09-11 17:51:37 -0300

    Trying to fix production only chart axis bug [3]

--- src/components/ChartType_Events.tsx
+++ src/components/ChartType_Events.tsx
@@ -145,7 +145,6 @@ const EventsChart = (props: { id: number; yAxisScale: [number, any] }) => {
               grid: { lineWidth: 2, borderWidth: 2 },
               beginAtZero: true,
               ticks: {
-                maxTicksLimit: 8,
                 // Prevent rare runtime RangeError
                 callback: function (val:string | number) {
                   return val;
